2d85310efedcf8315a0c8a03213af2d6b6fe3cef
Clifford <kingsleyclifford2016@gmail.com>
2021-10-16 20:50:30 +0000

    added lines of code

--- components/defaultComponent/landFilter.vue
+++ components/defaultComponent/landFilter.vue
@@ -67,97 +67,6 @@
         </div>
       </div>
     </div>
-    <div class="mt-4">
-      <div class="text-sm font-mono font-bold mb-2">Beds</div>
-      <div class="flex items-center justify-center gap-2">
-        <div
-          :class="[!selectedBed ? 'option-new !px-3' : 'option-old !px-3']"
-          @click="bed('')"
-        >
-          Any
-        </div>
-        <div
-          :class="[
-            selectedBed === '1+' ? 'option-new !px-3' : 'option-old !px-3',
-          ]"
-          @click="bed('1+')"
-        >
-          1+
-        </div>
-        <div
-          :class="[
-            selectedBed === '2+' ? 'option-new !px-3' : 'option-old !px-3',
-          ]"
-          @click="bed('2+')"
-        >
-          2+
-        </div>
-        <div
-          :class="[
-            selectedBed === '3+' ? 'option-new !px-3' : 'option-old !px-3',
-          ]"
-          @click="bed('3+')"
-        >
-          3+
-        </div>
-        <div
-          :class="[
-            selectedBed === '4+' ? 'option-new !px-3' : 'option-old !px-3',
-          ]"
-          @click="bed('4+')"
-        >
-          4+
-        </div>
-      </div>
-    </div>
-    <div class="mt-4">
-      <div class="text-sm font-mono font-bold mb-2">Bathrooms</div>
-      <div class="flex items-center justify-center gap-2">
-        <div
-          class="bathroom-select"
-          :class="[!selectedBathroom ? 'option-new !px-3' : 'option-old !px-3']"
-          @click="bathroom('')"
-        >
-          Any
-        </div>
-        <div
-          class="bathroom-select"
-          :class="[
-            selectedBathroom === '1+' ? 'option-new !px-3' : 'option-old !px-3',
-          ]"
-          @click="bathroom('1+')"
-        >
-          1+
-        </div>
-        <div
-          class="bathroom-select"
-          :class="[
-            selectedBathroom === '2+' ? 'option-new !px-3' : 'option-old !px-3',
-          ]"
-          @click="bathroom('2+')"
-        >
-          2+
-        </div>
-        <div
-          class="bathroom-select"
-          :class="[
-            selectedBathroom === '3+' ? 'option-new !px-3' : 'option-old !px-3',
-          ]"
-          @click="bathroom('3+')"
-        >
-          3+
-        </div>
-        <div
-          class="bathroom-select"
-          :class="[
-            selectedBathroom === '4+' ? 'option-new !px-3' : 'option-old !px-3',
-          ]"
-          @click="bathroom('4+')"
-        >
-          4+
-        </div>
-      </div>
-    </div>
     <div class="land-size">
       <div class="text-sm font-mono font-bold">Land Space</div>
       <div class="content">
@@ -175,46 +84,6 @@
             label="Max Size"
             :options="maxLandSizes"
           ></baseSelect>
-        </div>
-      </div>
-    </div>
-    <div class="lot-size">
-      <div class="text-sm font-mono font-bold">Lot Size</div>
-      <div class="content">
-        <div class="min-lot-size">
-          <baseSelect
-            v-model="selectedMinLotSize"
-            label="Min Size"
-            :options="minLotSizes"
-          ></baseSelect>
-        </div>
-        <div class="w-5 h-1 bg-gray-200 mt-8"></div>
-        <div class="max-lot-size">
-          <baseSelect
-            v-model="selectedMaxLotSize"
-            label="Max Size"
-            :options="maxLotSizes"
-          ></baseSelect>
-        </div>
-      </div>
-    </div>
-    <div class="year-built">
-      <div class="text-sm font-mono font-bold">Year Built</div>
-      <div class="content">
-        <div class="min-year">
-          <baseInput
-            v-model="selectedMinYear"
-            placeholder="Min Price"
-            label="Min Year"
-          ></baseInput>
-        </div>
-        <div class="w-5 h-1 bg-gray-200 mt-6"></div>
-        <div class="max-year">
-          <baseInput
-            v-model="selectedMaxYear"
-            placeholder="Max Price"
-            label="Max Year"
-          ></baseInput>
         </div>
       </div>
     </div>
@@ -237,26 +106,19 @@
 import baseSelect from '~/components/defaultComponent/baseSelect'
 import ButtonPrimary from '~/components/defaultComponent/button-primary.vue'
 import ButtonPrimaryOpen from '~/components/defaultComponent/button-primary-open.vue'
-import baseInput from '~/components/defaultComponent/baseInput'
+// import baseInput from '~/components/defaultComponent/baseInput'
 export default {
   components: {
     baseSelect,
     ButtonPrimary,
     ButtonPrimaryOpen,
-    baseInput,
   },
   data() {
     return {
-      selectedMinLandSize: this.$route.query.minLandSize || 'Any Size',
-      selectedMaxLandSize: this.$route.query.maxLandSize || 'Any Size',
-      selectedMinLotSize: this.$route.query.minLotSize || 'Any Size',
-      selectedMaxLotSize: this.$route.query.maxLotSize || 'Any Size',
-      selectedMinYear: this.$route.query.minYear || '',
-      selectedMaxYear: this.$route.query.maxYear || '',
       selectedMinPrice: this.$route.query.minPrice || 'Any Price',
       selectedMaxPrice: this.$route.query.maxPrice || 'Any Price',
-      selectedBed: this.$route.query.beds || null,
-      selectedBathroom: this.$route.query.bathrooms || null,
+      selectedMinLandSize: this.$route.query.minLandSize || 'Any Size',
+      selectedMaxLandSize: this.$route.query.maxLandSize || 'Any Size',
       minPrices: [
         'Any Price',
         'GH¢100',
@@ -343,104 +205,7 @@ export default {
       }
     },
   },
-  // watch: {
-  //   selectedMinPrice(value) {
-  //     this.$router.push({
-  //       query: {
-  //         minPrice: value,
-  //         maxPrice: this.$route.query.maxPrice,
-  //         bathrooms: this.$route.query.bathrooms,
-  //         beds: this.$route.query.beds,
-  //         minLandSize: this.$route.query.minLandSize,
-  //         maxLandSize: this.$route.query.maxLandSize,
-  //         minLotSize: this.$route.query.minLotSize,
-  //         maxLotSize: this.$route.query.maxLotSize,
-  //         minYear: this.$route.query.minYear,
-  //         maxYear: this.$route.query.maxYear,
-  //       },
-  //     })
-  //   },
-  //   selectedMaxPrice(value) {
-  //     this.$router.push({
-  //       query: {
-  //         maxPrice: value,
-  //         minPrice: this.$route.query.minPrice,
-  //         bathrooms: this.$route.query.bathrooms,
-  //         beds: this.$route.query.beds,
-  //         minLandSize: this.$route.query.minLandSize,
-  //         maxLandSize: this.$route.query.maxLandSize,
-  //         minLotSize: this.$route.query.minLotSize,
-  //         maxLotSize: this.$route.query.maxLotSize,
-  //         minYear: this.$route.query.minYear,
-  //         maxYear: this.$route.query.maxYear,
-  //       },
-  //     })
-  //   },
-  //   selectedMinLandSize(value) {
-  //     this.$router.push({
-  //       query: {
-  //         maxPrice: this.$route.query.maxPrice,
-  //         minPrice: this.$route.query.minPrice,
-  //         bathrooms: this.$route.query.bathrooms,
-  //         beds: this.$route.query.beds,
-  //         minLandSize: value,
-  //         maxLandSize: this.$route.query.maxLandSize,
-  //         minLotSize: this.$route.query.minLotSize,
-  //         maxLotSize: this.$route.query.maxLotSize,
-  //         minYear: this.$route.query.minYear,
-  //         maxYear: this.$route.query.maxYear,
-  //       },
-  //     })
-  //   },
-  //   selectedMaxLandSize(value) {
-  //     this.$router.push({
-  //       query: {
-  //         maxPrice: this.$route.query.maxPrice,
-  //         minPrice: this.$route.query.minPrice,
-  //         bathrooms: this.$route.query.bathrooms,
-  //         beds: this.$route.query.beds,
-  //         minLandSize: this.$route.query.minLandSize,
-  //         maxLandSize: value,
-  //         minLotSize: this.$route.query.minLotSize,
-  //         maxLotSize: this.$route.query.maxLotSize,
-  //         minYear: this.$route.query.minYear,
-  //         maxYear: this.$route.query.maxYear,
-  //       },
-  //     })
-  //   },
-  //   selectedMinLotSize(value) {
-  //     this.$router.push({
-  //       query: {
-  //         maxPrice: this.$route.query.maxPrice,
-  //         minPrice: this.$route.query.minPrice,
-  //         bathrooms: this.$route.query.bathrooms,
-  //         beds: this.$route.query.beds,
-  //         minLandSize: this.$route.query.minLandSize,
-  //         maxLandSize: this.$route.query.maxLandSize,
-  //         minLotSize: value,
-  //         maxLotSize: this.$route.query.maxLotSize,
-  //         minYear: this.$route.query.minYear,
-  //         maxYear: this.$route.query.maxYear,
-  //       },
-  //     })
-  //   },
-  //   selectedMaxLotSize(value) {
-  //     this.$router.push({
-  //       query: {
-  //         maxPrice: this.$route.query.maxPrice,
-  //         minPrice: this.$route.query.minPrice,
-  //         bathrooms: this.$route.query.bathrooms,
-  //         beds: this.$route.query.beds,
-  //         minLandSize: this.$route.query.minLandSize,
-  //         maxLandSize: this.$route.query.maxLandSize,
-  //         minLotSize: this.$route.query.minLotSize,
-  //         maxLotSize: value,
-  //         minYear: this.$route.query.minYear,
-  //         maxYear: this.$route.query.maxYear,
-  //       },
-  //     })
-  //   },
-  // },
+
   methods: {
     numFormatter(num) {
       if (num >= 1000000000) {
@@ -453,80 +218,35 @@ export default {
       return num
     },
 
-    bed(num) {
-      // this.$router.push({
-      //   query: {
-      //     beds: num,
-      //     bathrooms: this.$route.query.bathrooms,
-      //     minPrice: this.$route.query.minPrice,
-      //     maxPrice: this.$route.query.maxPrice,
-      //     minLandSize: this.$route.query.minLandSize,
-      //     maxLandSize: this.$route.query.maxLandSize,
-      //     minLotSize: this.$route.query.minLotSize,
-      //     maxLotSize: this.$route.query.maxLotSize,
-      //     minYear: this.$route.query.minYear,
-      //     maxYear: this.$route.query.maxYear,
-      //   },
-      // })
-      this.selectedBed = num
-    },
-    bathroom(num) {
-      // this.$router.push({
-      //   query: {
-      //     bathrooms: num,
-      //     beds: this.$route.query.beds,
-      //     minPrice: this.$route.query.minPrice,
-      //     maxPrice: this.$route.query.maxPrice,
-      //     minLandSize: this.$route.query.minLandSize,
-      //     maxLandSize: this.$route.query.maxLandSize,
-      //     minLotSize: this.$route.query.minLotSize,
-      //     maxLotSize: this.$route.query.maxLotSize,
-      //     minYear: this.$route.query.minYear,
-      //     maxYear: this.$route.query.maxYear,
-      //   },
-      // })
-      this.selectedBathroom = num
-    },
     filter() {
       this.$router.push({
         path: this.$router.currentRoute.fullPath,
         query: {
-          bathrooms: this.selectedBathroom,
-          beds: this.selectedBed,
           minPrice: this.selectedMinPrice,
           maxPrice: this.selectedMaxPrice,
           minLandSize: this.selectedMinLandSize,
           maxLandSize: this.selectedMaxLandSize,
           minLotSize: this.selectedMinLotSize,
           maxLotSize: this.selectedMaxLotSize,
-          minYear: this.selectedMinYear,
-          maxYear: this.selectedMaxYear,
         },
       })
-      console.log(this.$router.currentRoute)
     },
     reset() {
       this.$router.push({
         path: 'homes/',
         query: {
-          bathrooms: null,
-          beds: null,
           minPrice: null,
           maxPrice: null,
           minLandSize: null,
           maxLandSize: null,
           minLotSize: null,
           maxLotSize: null,
-          minYear: null,
-          maxYear: null,
         },
       })
       this.selectedMinLandSize = 'Any Size'
       this.selectedMaxLandSize = 'Any Size'
       this.selectedMinLotSize = 'Any Size'
       this.selectedMaxLotSize = 'Any Size'
-      this.selectedMinYear = ''
-      this.selectedMaxYear = ''
       this.selectedMinPrice = 'Any Price'
       this.selectedMaxPrice = 'Any Price'
     },
