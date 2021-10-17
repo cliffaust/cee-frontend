94cca7d5d0e747e59c5f2adaae6f88e5c4854449
Clifford <kingsleyclifford2016@gmail.com>
2021-10-17 21:29:10 +0000

    added lines of code

--- components/defaultComponent/percentage-bar.vue
+++ components/defaultComponent/percentage-bar.vue
@@ -15,17 +15,18 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="postcss" scoped>
 .bar {
   background-color: $dark-gray;
   width: 100%;
   height: 7px;
   border-radius: 100px;
-  .percent {
-    background-color: $primary-bgcolor-2;
-    width: var(--percent);
-    height: 7px;
-    border-radius: 100px;
-  }
+}
+
+.percent {
+  background-color: $primary-bgcolor-2;
+  width: var(--percent);
+  height: 7px;
+  border-radius: 100px;
 }
 </style>
